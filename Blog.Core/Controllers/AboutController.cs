eedc41ad100e8e1c0dd08584f87efe4ee79a7a6d
Marcus Weaver <onemangd@gmail.com>
2018-06-24 12:34:54 +0300

    Moving

--- Blog.Core/Controllers/AboutController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Blog.Core.Controllers
-{
-    public class AboutController : Controller
-    {
-        public IActionResult Index() => View("About");
-    }
-}
