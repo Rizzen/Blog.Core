e887afb254696b66652ab8733bb9d02bbfa0221f
kewlu <pampam@my.com>
2019-08-20 23:57:43 +0300

    fix async initialization in InitialStateCreator

---
+++ src/Blog.Core/Utils/IAsyncInitialization.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Utils
+{
+    public interface IAsyncInitialization
+    {
+        Task Initialization { get; }
+    }
+}
--- src/Blog.Core/Utils/InitialStateCreator.cs
+++ src/Blog.Core/Utils/InitialStateCreator.cs
@@ -1,10 +1,11 @@
-﻿using Blog.Core.Caching.Caching;
+﻿using System.Threading.Tasks;
+using Blog.Core.Caching.Caching;
 using Blog.Core.DAL.Posts;
 using Blog.Core.Metadata;
 
 namespace Blog.Core.Utils
 {
-    public class InitialStateCreator
+    public class InitialStateCreator : IAsyncInitialization
     {
         private readonly IPostCache _cache;
         private readonly IPostStore _store;
@@ -15,13 +16,17 @@ namespace Blog.Core.Utils
             _cache = cache;
             _store = store;
             _metadata = metadata;
+
+            Initialization = InitializeAsync();
         }
 
-        // BAD
-        public async void Init()
+
+        public Task Initialization { get;  }
+
+        private async Task InitializeAsync()
         {
             var posts = _store.GetAllPostsWithNames();
-            var postsWithMeta =  await _metadata.GetMetadataForPosts(posts);
+            var postsWithMeta = await _metadata.GetMetadataForPosts(posts);
             _cache.Store(postsWithMeta);
         }
     }
